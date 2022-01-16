f6ee9cca015dd4e31e3c69dcdcd0c1828eba231e
EssentialAccident <linooropeza@gmail.com>
2022-01-15 23:57:57 -0500

    Added some constants and some logic to test the square logic

--- ballz.rb
+++ ballz.rb
@@ -10,11 +10,13 @@ require 'ruby2d'
 require 'vector2d'
 
 # Require local dependecies
-
+require_relative 'lib/my_square'
 ##  Main Program  ##
 
 # Constants
-SQUARE_SIZE = 64
+SQUARE_SIZE = 64 # Size of the squares of the game
+SQUARE_DIVISION = 3 # Separation between squares
+FONT_SIZE = SQUARE_SIZE.to_f / 2.5 # Size of the font of the counter in the square
 
 # Setting up the window
 set({
@@ -37,6 +39,14 @@ end
 # Update loop
 update do
 end
+
+my_square = MySquare.new({
+                           position: Vector2d.new(3, 3),
+                           z: 1,
+                           size: SQUARE_SIZE,
+                           color: 'red',
+                           count: 5
+                         })
 
 # Show the window
 show
