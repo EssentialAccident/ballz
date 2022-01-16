16abcff7017a2607fd2c08ba607a84121aa3775c
EssentialAccident <linooropeza@gmail.com>
2022-01-15 23:54:41 -0500

    Added the count and the text  to the square

--- lib/my_square.rb
+++ lib/my_square.rb
@@ -3,10 +3,31 @@
 # This class will manage the logic for the squares
 class MySquare < Ruby2D::Square
   def initialize(opts = {})
+    # Initializing the square
     @position = opts[:position]
+    @count = opts[:count]
     opts[:x] = @position.x
     opts[:y] = @position.y
     super(opts)
-    @count = opts[:count]
+
+    # Initializing the text
+    @count_text = Text.new(
+      @count.to_s,
+      {
+        size: FONT_SIZE,
+        color: 'white',
+        z: @z + 3
+      }
+    )
+    calculate_text_position
+  end
+
+  private
+
+  def calculate_text_position
+    x = @x + (SQUARE_SIZE / 2) - (FONT_SIZE * @count.to_s.length / 2)
+    y = ((@y + SQUARE_SIZE) / 2) - (FONT_SIZE / 2)
+    @count_text.x = x
+    @count_text.y = y
   end
 end
