9990f9a30671a8dec61327d48cb92b66ca4398f7
Lino Oropeza <linooropeza@gmail.com>
2022-01-22 12:37:07 -0500

    MySquare now account for its position on the grid and on the screen

--- lib/my_square.rb
+++ lib/my_square.rb
@@ -2,12 +2,19 @@
 
 # This class will manage the logic for the squares
 class MySquare < Ruby2D::Square
+  # @screen_position is a Vector2d object that contains the position of the square on the screen
+  # @grid_position is a Vecotor2d object that contains the position of the square in the grid
+  # The grid is defined by SQUARES_PER_LINE and SQUARES_PER_COLUMN
+
   def initialize(opts = {})
+    # opts[:position] is the position of the square on the grid
     # Initializing the square
-    @position = opts[:position]
+    grid_position(opts[:grid_position])
     @count = opts[:count]
-    opts[:x] = @position.x
-    opts[:y] = @position.y
+    opts[:x] = @screen_position.x
+    opts[:y] = @screen_position.y
+    opts[:color] = 'red'
+    opts[:size] = SQUARE_SIZE
     super(opts)
 
     # Initializing the text
@@ -20,6 +27,14 @@ class MySquare < Ruby2D::Square
       }
     )
     calculate_text_position
+  end
+
+  def grid_position(position)
+    @grid_position = position
+    @screen_position = Vector2d.new(
+      (position.x * SQUARE_SIZE) + ((position.x + 1) * SQUARE_DIVISION),
+      (position.y * SQUARE_SIZE) + ((position.y + 1) * SQUARE_DIVISION)
+    )
   end
 
   private
