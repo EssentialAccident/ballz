4d1ba9ce37d5bf3138d88f04f9e3e7a25319b5ca
Lino Oropeza <linooropeza@gmail.com>
2022-01-22 12:37:25 -0500

    It adds a level

--- lib/squares.rb
+++ lib/squares.rb
@@ -12,17 +12,45 @@ class Squares
   end
 
   # Every time the player gets to a new level this method will add the necessary squares to matrix
-  def add_level(_level)
+  def add_level(level)
     # Sets how many squares will be add to the line
-    count = rand(2..SQUARES_PER_LINE)
+    num_squares = rand(2..SQUARES_PER_LINE)
 
     # Set the position of those squares in the line
-    position = []
-    while position.count < count
+    positions = []
+    while positions.count < num_squares
       try = rand(SQUARES_PER_LINE)
-      position.append(try) unless position.include?(try)
+      positions.append(try) unless positions.include?(try)
     end
-    puts count
-    pp position
+
+    # Set up the count for the squares
+    # 70% of the time the count will be equal to the level
+    # 26% of the time the count will twice the value fo the level
+    #  4% of the time the count will be three times the value of the level
+    counts = []
+    positions.count.times do
+      case rand(100)
+      when 0..70
+        try = level
+      when 71..95
+        try = level * 2
+      when 96..100
+        try = level * 3
+      end
+      counts.append(try)
+    end
+
+    # Add the squares to the @squares matrix
+    add = []
+    positions.each_index do |index|
+      add.append(MySquare.new(
+                   grid_position: Vector2d.new(positions[index], 1),
+                   count: counts[index]
+                 ))
+    end
+    puts num_squares
+    puts level
+    pp positions.sort
+    pp counts
   end
 end
